fad0601ffe45db3bb6283b9c1b6d03f7fcf227eb
Anand <anandkumar01.2020@gmail.com>
2026-03-26 18:20:32 +0530

    adding

--- src/main/java/com/codingshuttle/projects/lovable_clone/mapper/ProjectMapper.java
+++ src/main/java/com/codingshuttle/projects/lovable_clone/mapper/ProjectMapper.java
@@ -4,6 +4,7 @@ import com.codingshuttle.projects.lovable_clone.dto.project.ProjectResponse;
 import com.codingshuttle.projects.lovable_clone.dto.project.ProjectSummaryResponse;
 import com.codingshuttle.projects.lovable_clone.entity.Project;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ public interface ProjectMapper {
    ProjectResponse toProjectResponse(Project project);
 
 
+   @Mapping(target = "projectName", source= "name")
    ProjectSummaryResponse toProjectSummaryResponse(Project project);
 
    List<ProjectSummaryResponse> toListOfProjectSummaryResponse(List<Project> projects);
